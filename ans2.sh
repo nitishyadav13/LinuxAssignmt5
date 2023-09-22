ff3156a7f26532bd9b8862d2288417574d9162a8
NITISHYADAV <68838610+nitishyadav13@users.noreply.github.com>
2023-09-22 12:37:21 +0530

    Add files via upload

---
+++ ans2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Hero Name:"
+read h
+echo "Movie Name"
+read m
+echo "Language :"
+read l
+echo "Price :"
+read p
+echo "ReleaseDate"
+read rd
+
+echo "Hero'sName: $h "
+echo "Movie'sName: $m "
+echo "Language: $l"
+echo "Movie'sPrice: $p "
+echo "Movie'sReleaseDate: $rd "
