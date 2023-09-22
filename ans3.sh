4d4438722a504ebf14029100b047fcc1081425a5
NITISHYADAV <68838610+nitishyadav13@users.noreply.github.com>
2023-09-22 21:20:48 +0530

    Add files via upload

---
+++ ans3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+i=0
+echo "Entre a number:"
+echo ""
+while((i<=999))
+
+do
+c=$i
+d=$i
+b=0
+a=0
+while((c>0))
+do
+   a=$((c%10))
+   b=$((b+a*a*a))
+   c=$((c/10))
+done
+if((b==d));
+then
+echo "$i"
+fi
+i=$((i+1))
+done
+echo ""
+
